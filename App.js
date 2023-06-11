fac360c0febb06d817ecfea5e441a27cead767f9
Shweta Rajendra Saindane <80962110+shweta27-alt@users.noreply.github.com>
2023-06-11 12:01:35 +0530

    Basic tags till scrollview

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, View , Image, TextInput, Button, Switch , ToastAndroid, ScrollView} from 'react-native';
+import {useState} from 'react';
+import Home from './Screens/Home'
 
 export default function App() {
+  const [active , setActive] = useState(false)
+  const showToast = () =>{
+    ToastAndroid.show("Hey I'm toast please do remember me", ToastAndroid.SHORT)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <ScrollView>
+      <SafeAreaView>
+      <Home/>
+      <Image source={{uri:"https://en.wikipedia.org/wiki/Blue-and-yellow_macaw#/media/File:Blue-and-Yellow-Macaw.jpg"}} style={{width:100, height:100,backgroundColor:"red"}}/>
+      <TextInput style={{backgroundColor:"blue"}}/>
+      <Button title="Click me" onPress={showToast} color="pink"/>
+      <Button title="Click me" onPress={showToast} color="black"/>
+      <Button title="Click me" onPress={showToast} color="red"/>
+      <Button title="Click me" onPress={showToast} color="yellow"/>
+      <Button title="Click me" onPress={showToast} color="pink"/>
+      <Button title="Click me" onPress={showToast} color="black"/>
+      <Button title="Click me" onPress={showToast} color="red"/>
+      <Button title="Click me" onPress={showToast} color="yellow"/>
+      <Button title="Click me" onPress={showToast} color="pink"/>
+      <Button title="Click me" onPress={showToast} color="black"/>
+      <Button title="Click me" onPress={showToast} color="red"/>
+      <Button title="Click me" onPress={showToast} color="yellow"/>
+      <Button title="Click me" onPress={showToast} color="pink"/>
+      <Button title="Click me" onPress={showToast} color="black"/>
+      <Button title="Click me" onPress={showToast} color="pink"/>
+      <Button title="Click me" onPress={showToast} color="black"/>
+      <Button title="Click me" onPress={showToast} color="red"/>
+      <Button title="Click me" onPress={showToast} color="yellow"/>
+      <Button title="Click me" onPress={showToast} color="pink"/>
+      <Button title="Click me" onPress={showToast} color="black"/>
+      <Button title="Click me" onPress={showToast} color="red"/>
+      <Button title="Click me" onPress={showToast} color="yellow"/>
+      <Button title="Click me" onPress={showToast} color="red"/>
+      <Button title="Click me" onPress={showToast} color="yellow"/>
+      <Button title="Click me" onPress={showToast} color="pink"/>
+      <Button title="Click me" onPress={showToast} color="black"/>
+      <Button title="Click me" onPress={showToast} color="red"/>
+      <Button title="Click me" onPress={showToast} color="yellow"/>
+      <Switch value={active} onChange={()=>{setActive( (prev)=> !prev)}}/>
+      </SafeAreaView>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
